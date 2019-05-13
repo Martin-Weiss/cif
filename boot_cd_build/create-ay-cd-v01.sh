8be1300c34b50108d68a93c96f817eaca0545f15
root <martin.weiss@suse.com>
2019-05-13 18:24:18 +0200

    added efi for boot cd

--- boot_cd_build/create-ay-cd-v01.sh
+++ boot_cd_build/create-ay-cd-v01.sh
@@ -7,7 +7,8 @@
 #
 # create-ay-cd-v01.sh 					27 Apr 2013
 # change copy to move for backup			25 Feb 2019
-# last modified (Customer Name)				25 Feb 2019
+# add efi boot via grub2				13 May 2019
+# last modified (Customer Name)				13 May 2019
 
 
 ARGV=$@
@@ -79,16 +80,31 @@ function set_vars()
 	ISO_DIR=$DEF_ISO_DIR
 	MY_DATE=$(date +%Y-%m-%d_%H%M)
 	ISO_NAME_COPIED=$ISO_PREF.iso.$MY_DATE
-	ISO_LABEL="AUTOYAST-$CUSTOMER"
+	ISO_LABEL="AUTOYAST_$CUSTOMER"
+	TEMP_DIR="/tmp"
 }
 
+function prepare_efi()
+{
+	grub2-mkstandalone --format=x86_64-efi --output=$TEMP_DIR/bootx64.efi --locales="" --fonts="" "boot/grub/grub.cfg=$WORK_DIR/grub2.cfg" --modules="efi_gop efi_uga all_video gzio gettext gfxterm gfxmenu png"
+	dd if=/dev/zero of=$TEMP_DIR/efiboot.img bs=1M count=10 && mkfs.vfat $TEMP_DIR/efiboot.img && mmd -i $TEMP_DIR/efiboot.img efi efi/boot && mcopy -i $TEMP_DIR/efiboot.img $TEMP_DIR/bootx64.efi ::efi/boot/
+}
 
 function make_iso()
 {
 	test -f "$ISO_DIR/$ISO_NAME" && mv "$ISO_DIR/$ISO_NAME" $DEF_ISO_DIR/$ISO_NAME_COPIED
 	
-	mkisofs -f -rV "$ISO_LABEL" -b boot/grub/iso9660_stage1_5 -no-emul-boot -boot-load-size \
-        4 -boot-info-table -D -o $ISO_DIR/$ISO_NAME $WORK_DIR
+	xorriso -as mkisofs \
+	-volid "$ISO_LABEL" \
+	-full-iso9660-filenames \
+	-J -joliet-long -l -R -rock \
+	-no-emul-boot -boot-load-size 4 \
+	-boot-info-table \
+	-iso-level 4 \
+	-b boot/grub/iso9660_stage1_5 -no-emul-boot -boot-load-size 4 -boot-info-table \
+	-eltorito-alt-boot -e EFI/efiboot.img -no-emul-boot -append_partition 2 0xef $TEMP_DIR/efiboot.img \
+	-output $ISO_DIR/$ISO_NAME \
+ 	--graft-points "$WORK_DIR" "/EFI/efiboot.img=$TEMP_DIR/efiboot.img"
 }	
 
 function make_info()
@@ -101,12 +117,20 @@ You will find the IMAGE here: $ISO_DIR/$ISO_NAME
 HERE
 }
 
+function clean_up()
+{
+	rm /tmp/efiboot.img /tmp/bootx64.efi
+}
+
 function main()
 {
 	set_vars
 	getopt  $ARGV
+	prepare_efi
 	make_iso
 	make_info
+	clean_up
 }
 
 main
+
