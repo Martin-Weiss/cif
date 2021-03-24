231f08ae22ed6452711210fbaa25e37a49951967
root <martin.weiss@suse.com>
2021-03-24 10:19:52 +0100

    correct typo in destroy.sh

--- terraform/destroy.sh
+++ terraform/destroy.sh
@@ -6,7 +6,7 @@ terraform destroy
 
 for SERVER in $(cat server.txt|grep -v servername|cut -f1 -d ","); do
         echo deleting $SERVER
-	DOMAIN=$(grep $SERVER server.txt|cut -f6 -s ",")
+	DOMAIN=$(grep $SERVER server.txt|cut -f6 -d ",")
         /usr/bin/spacecmd -y -- system_delete -c NO_CLEANUP $SERVER.$DOMAIN
         sed -i "/$SERVER/d" /root/.ssh/known_hosts
 done
