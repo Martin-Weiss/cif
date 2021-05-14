4545c7a762c5b8351c5c8511e639e7e6e4fa2600
root <martin.weiss@suse.com>
2021-05-14 11:38:23 +0200

    ouput.tf handling server that is off, destroy.sh delete ip

--- terraform/output.tf
+++ terraform/output.tf
@@ -3,6 +3,6 @@ output "ip_servers" {
    #vsphere_virtual_machine.server[each.key].guest_ip_addresses.0
    value = {
         for instance in vsphere_virtual_machine.server:
-        instance.name => instance.guest_ip_addresses.0
+        instance.name => try(instance.guest_ip_addresses.0, "")
    }
 }
